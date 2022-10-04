bacca73290f26849845b532bf57fea2125a3fdab
Proggery <80548375+Proggery@users.noreply.github.com>
2022-10-04 20:32:09 +0200

    sda

--- src/components/SelectOption/SelectOption.styled.ts
+++ src/components/SelectOption/SelectOption.styled.ts
@@ -8,13 +8,6 @@ export const SelectContainer = styled.span`
   * {
     color: white !important;
   }
-  .css-1rxz5jq-MuiSelect-select-MuiInputBase-input-MuiInput-input.css-1rxz5jq-MuiSelect-select-MuiInputBase-input-MuiInput-input.css-1rxz5jq-MuiSelect-select-MuiInputBase-input-MuiInput-input {
-    padding-left: 3px;
-  }
-
-  .css-a3l6o-MuiInputBase-root-MuiInput-root-MuiSelect-root:after {
-    border-color: ${colors.primaryColor};
-  }
 
   svg {
     color: white;
