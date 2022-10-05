667e90c96eb4fb574221c486baa2a6a29665941d
Proggery <80548375+Proggery@users.noreply.github.com>
2022-10-05 10:15:58 +0200

    asd

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -5,7 +5,4 @@ import styled from "styled-components";
 export const AppContainer = styled.div`
   background: ${colors.backgroundColor};
   min-height: 100vh;
-  .css-r8u8y9 {
-    background: red;
-  }
 `;
